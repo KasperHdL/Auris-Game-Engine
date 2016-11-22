b8048b33efd64297f4288c58d49f0251a448ac28
Mathias Hedelund <mdhe@itu.dk>
2016-11-22 15:49:49 +0100

    Changed SceneParser.hpp

--- src/Systems/SceneParser.hpp
+++ src/Systems/SceneParser.hpp
@@ -2,3 +2,28 @@
 
 #include "picojson.h"
 #include <fstream>
+
+using namespace std;
+
+class GameObjectDescriptor {
+public:
+    // Uniquely defines a game object
+    int uniqueId;
+
+    string name;
+
+    glm::vec4 color;
+
+    // transform
+    // position relative to parent
+    glm::vec3 position;
+
+    // rotation relative to parent
+    glm::vec3 rotationEuler;
+
+    // scale relative to parent
+    glm::vec3 scale;
+
+    // parent id (-1 means no parent)
+    int parentId;
+};
