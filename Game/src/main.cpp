b815e967cfe614dc9200cfc4c69e018d625ab477
Mathias Hedelund <mdhe@itu.dk>
2016-12-12 12:41:22 +0100

    Added command line argument to main (window size)

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -8,14 +8,8 @@ DemoGame game;
 
 int main(int argc, char** argv) {
     // Use windows coordinates in this example (lower left corner is (0,0), upper right corner is (width, height)
-    SDL_DisplayMode* mode;
-    Uint32 f = mode->format;
-
-    float screenWidth = mode->w;
-    float screenHeight = mode->h;
-
-    float width = 1920;
-    float height = 1080;
+    float width = argv.length() > 0 ? argv[0][0] : 1920;
+    float height = argv.length() > 0 ? argv[0][1] : 1080;
 
     Engine engine(width, height);
     engine.startup(&game);
