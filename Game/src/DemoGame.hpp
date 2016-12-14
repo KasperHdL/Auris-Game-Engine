87c140ade3bd85a136c23b003f9994c8f294050f
Mathias Hedelund <mdhe@itu.dk>
2016-12-14 10:27:18 +0100

    Fixed stuff in AudioPlayer

--- Game/src/DemoGame.hpp
+++ Game/src/DemoGame.hpp
@@ -5,13 +5,16 @@
 #include "Scenes/Scene1.hpp"
 #include "Scenes/Scene2.hpp"
 
+
 class DemoGame : public Auris::Game {
     Scene1 scene1;
-
+    AudioPlayer audioPlayer;
+    int pistolShot;
 
     void init() {
         Auris::Engine::loadScene(&scene1);
         Auris::Engine::world->SetGravity(b2Vec2(0, -9.8));
+        pistolShot = audioPlayer.addSoundEffect("../../data/pistolShot.wav", 1.0f);
     }
 
     void earlyUpdate(float dt){
@@ -19,7 +22,9 @@ class DemoGame : public Auris::Game {
     }
 
     void update(float dt){
-
+        if (Input::keyDown(SDL_SCANCODE_SPACE)){
+            audioPlayer.playSoundEffect(pistolShot);
+        }
     }
 
     void lateUpdate(float dt){
