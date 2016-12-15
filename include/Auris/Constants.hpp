a05cf94ade0bdac5b52e701f468b4913d9176ee4
Jonas VHM <jonasvhm@gmail.com>
2016-12-15 10:34:57 +0100

    The first test comments

--- include/Auris/Constants.hpp
+++ include/Auris/Constants.hpp
@@ -2,8 +2,9 @@
 
 
 namespace Auris{
+//! A namespace for constants
 namespace Constants{
-    const float METERS_TO_PIXELS = 10.0f; //number of pixels per meter
-    const float PIXELS_TO_METERS = 1.0f / METERS_TO_PIXELS; //meters per pixel
+    const float METERS_TO_PIXELS = 10.0f; /*!< number of pixels per meter*/
+    const float PIXELS_TO_METERS = 1.0f / METERS_TO_PIXELS; /*!< meters per pixel*/
 }
 }
--- include/Auris/Engine.hpp
+++ include/Auris/Engine.hpp
@@ -40,40 +40,56 @@
 
 namespace Auris{
 class DebugUI;
+
+//! The game engine class
+    /*! Everything happens here */
 class Engine{
 public:
+    //! The engine contructor taking two arguments.
+        /*!
+          \param width an integer, that is the width of the window.
+          \param height an interger, that is the height of the window.
+        */
     Engine(int width, int height):width(width),height(height){}
+
+    //! The engine decontructor.
+        /*! Not used for anything, because when shutdown is run, everything is closed anyway
+            \sa shutdown()
+        */
     virtual ~Engine(){}
 
+    //! A method to startup the engine, taking 1 argument.
+        /*! Starts up all the engine systems
+          \param game a Game pointer, that is the current game.
+        */
     void startup(Game* game);
+
+    //! A method to shutdown the engine.
+        /*! Closes down all the engine systems */
     void shutdown();
 
+    //! A method to run the engine, taking 1 argument.
+        /*! This is the engine update, it runs once each frame
+          \param window a SDL_Window pointer, that is the current window of the game.
+        */
     void run(SDL_Window* window);
 
-    void HandleSDLEvents();
+    static b2World* world; /*!< A static b2World pointer: world. A reference to the physics world */
 
-    static b2World* world;
+    Game* game; /*!< A Game pointer: game. A reference to the current game */
 
-    static void loadScene(Scene* scene);
+    SDL_Window *window; /*!< A SDL_Window pointer: window. A reference to the current window */
 
-    Game* game;
+    RenderSystem renderSystem; /*!< A Rendersystem value: renderSystem. The rendersystem of the engine */
+    DebugUI* debugUI; /*!< A DebugUI pointer: debugUI. A reference to the current engine debugUI */
 
-    SDL_Window *window;
+    b2ContactListener* collisionHandler; /*!< A b2ContactListener pointer: collisionHandler. A reference to the box2d contactlistener, handling collision */
 
-    RenderSystem renderSystem;
-    DebugUI* debugUI;
+    const int VELOCITY_ITERATIONS = 16; /*!< A constant int value: VELOCITY_ITERATIONS. The velocity iterations of the physics world per frame */
+    const int POSITION_ITERATIONS = 16; /*!< A constant int value: POSITION_ITERATIONS. The position iterations of the physics world per frame */
 
-    b2ContactListener* collisionHandler;
-
-    const int VELOCITY_ITERATIONS = 16;
-    const int POSITION_ITERATIONS = 16;
-
-    int width;
-    int height;
- 
-    int quit;
-
-	MemoryLeakDetector memLeakDet;
+    int width; /*!< An int value: width. The current width of the window */
+    int height; /*!< An int value: height. The current height of the window */
 
 };
 }
