e8410b86c5d3394adffd3ef891920a0efce965c8
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2016-12-16 12:12:18 +0100

    Default Cube Mesh

--- include/Auris/Entities/Nuggets/Sprite.hpp
+++ include/Auris/Entities/Nuggets/Sprite.hpp
@@ -42,11 +42,11 @@ public:
          * \param entity an Entity pointer, that is the entity the sprite is associated with.
          * \param texture a Texture pointer, that is the texture of the sprite (default is white).
          * \param normalMap a Texture pointer, that is the normal map of the sprite (default is no normal map).
-         * \param mesh a Mesh pointer, that is the mesh of the sprite (default is quad mesh).
+         * \param mesh a Mesh pointer, that is the mesh of the sprite (default is cube mesh).
         */
     Sprite(Entity* entity, SRE::Texture* texture = nullptr, SRE::Texture* normalMap = nullptr, Mesh* mesh = nullptr):Nugget(entity){
         if(mesh == nullptr)
-            this->mesh = Mesh::createQuad();
+            this->mesh = Mesh::createCube();
         else
             this->mesh = mesh;
 
@@ -70,7 +70,7 @@ public:
          * Initializes the mesh and the material of the sprite to values inputted.
          * \param entity an Entity pointer, that is the entity the sprite is associated with.
          * \param texture a Texture pointer, that is the texture of the sprite (default is white).
-         * \param mesh a Mesh pointer, that is the mesh of the sprite (default is quad mesh).
+         * \param mesh a Mesh pointer, that is the mesh of the sprite (default is cube mesh).
          * \overload Sprite(Entity* entity, SRE::Texture* texture = nullptr, SRE::Texture* normalMap = nullptr, Mesh* mesh = nullptr)
         */
     Sprite(Entity* entity, SRE::Texture* texture, Mesh* mesh = nullptr):Nugget(entity){
