99ed503c6cba65d273bb955585032bd14605db6e
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2016-12-16 11:35:09 +0100

    Buttons more like the used to

--- include/Auris/Action.hpp
+++ include/Auris/Action.hpp
@@ -10,10 +10,10 @@ namespace Action{
     const SDL_Scancode right           = SDL_SCANCODE_RIGHT;
     const SDL_Scancode quit            = SDL_SCANCODE_ESCAPE;
 
-    const SDL_Scancode debug_menu      = SDL_SCANCODE_F1;
-    const SDL_Scancode toggleInspector = SDL_SCANCODE_F2;
-    const SDL_Scancode toggleHierarchy = SDL_SCANCODE_F3;
-    const SDL_Scancode toggleProfiler  = SDL_SCANCODE_F4;
+    const SDL_Scancode debug_menu      = SDL_SCANCODE_F2;
+    const SDL_Scancode toggleInspector = SDL_SCANCODE_F3;
+    const SDL_Scancode toggleHierarchy = SDL_SCANCODE_F4;
+    const SDL_Scancode toggleProfiler  = SDL_SCANCODE_F5;
 
     const SDL_Scancode drawColliders   = SDL_SCANCODE_F7;
     const SDL_Scancode drawDebug       = SDL_SCANCODE_F8;
